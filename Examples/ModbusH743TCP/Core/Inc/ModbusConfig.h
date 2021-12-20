644032316ebc9af02c6e6071854be9d81923bbd8
Henning Mende <h.mende@pionierkraft.de>
2021-12-20 17:24:47 +0100

    add missing defines to ModbusConfig.h

--- Examples/ModbusH743TCP/Core/Inc/ModbusConfig.h
+++ Examples/ModbusH743TCP/Core/Inc/ModbusConfig.h
@@ -26,6 +26,12 @@
 #define MAX_M_HANDLERS 2    //Maximum number of modbus handlers that can work concurrently
 #define MAX_TELEGRAMS 2     //Max number of Telegrams in master queue
 
+// Values copied from the ModbusF429TCP example project.
+#define NUMBERTCPCONN   4   // Maximum number of simultaneous client connections, it should be equal or less than LWIP configuration
+#define TCPAGINGCYCLES	1000 // Number of times the master will check for a incoming request before closing the connection for inactivity
+/* Note: the total aging time for a connection is approximately NUMBERTCPCONN*TCPAGINGCYCLES*u16timeOut ticks
+ * for the values selected in this example it is approximately 40 seconds
+*/
 
 
 
